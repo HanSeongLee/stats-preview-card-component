9dff2766453e6940b79910a33a83413f76f93676
HanSeong Lee <jkhanseong@gmail.com>
2021-12-25 23:31:44 +0900

    Finished

---
+++ components/PreviewCard/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+const PreviewCard = () => {
+    return (
+        <div className={styles.previewCard}>
+            <div className={styles.cardHeader}>
+                <picture>
+                    <source srcSet="/img/image-header-mobile.jpg"
+                            media="(max-width: 568px)"/>
+                    <img className={styles.cardHeaderImage}
+                         src={'/img/image-header-desktop.jpg'}
+                         alt={''}
+                    />
+                </picture>
+            </div>
+            <div className={styles.cardBody}>
+                <h1 className={styles.title}>
+                    Get <strong>insights</strong> that help your business grow.
+                </h1>
+                <p className={styles.description}>
+                    Discover the benefits of data analytics and make better decisions regarding revenue, customer
+                    experience, and overall efficiency.
+                </p>
+
+                <div className={styles.statsContainer}>
+                    <div className={styles.statsWrapper}>
+                        <div className={styles.statsNumberText}>
+                            10k+
+                        </div>
+                        <div className={styles.statsNameText}>
+                            companies
+                        </div>
+                    </div>
+                    <div className={styles.statsWrapper}>
+                        <div className={styles.statsNumberText}>
+                            314
+                        </div>
+                        <div className={styles.statsNameText}>
+                            templates
+                        </div>
+                    </div>
+                    <div className={styles.statsWrapper}>
+                        <div className={styles.statsNumberText}>
+                            12m+
+                        </div>
+                        <div className={styles.statsNameText}>
+                            queries
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PreviewCard;
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,64 @@
 import Head from 'next/head';
+import styles from './style.module.scss';
+import PreviewCard from "../components/PreviewCard";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport"
-              content="width=device-width, initial-scale=1.0"
-        />
+      <>
+        <Head>
+          <meta charSet="UTF-8"/>
+          <meta name="viewport"
+                content="width=device-width, initial-scale=1.0"
+          />
 
-        <title>{process.env.NEXT_PUBLIC_TITLE}</title>
-        <meta name="description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
+          <title>{process.env.NEXT_PUBLIC_TITLE}</title>
+          <meta name="description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
 
-        <meta name="description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
+          <meta name="description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
 
-        <meta property="og:url"
-              content={process.env.NEXT_PUBLIC_URL}
-        />
-        <meta property="og:type"
-              content="website"
-        />
-        <meta property="og:title"
-              content={process.env.NEXT_PUBLIC_TITLE}
-        />
-        <meta property="og:description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
-        <meta property="og:image"
-              content={process.env.NEXT_PUBLIC_OG_IMAGE}
-        />
+          <meta property="og:url"
+                content={process.env.NEXT_PUBLIC_URL}
+          />
+          <meta property="og:type"
+                content="website"
+          />
+          <meta property="og:title"
+                content={process.env.NEXT_PUBLIC_TITLE}
+          />
+          <meta property="og:description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
+          <meta property="og:image"
+                content={process.env.NEXT_PUBLIC_OG_IMAGE}
+          />
 
-        <meta name="twitter:card"
-              content="summary_large_image"
-        />
-        <meta property="twitter:url"
-              content={process.env.NEXT_PUBLIC_URL}
-        />
-        <meta name="twitter:title"
-              content={process.env.NEXT_PUBLIC_TITLE}
-        />
-        <meta name="twitter:description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
-        <meta name="twitter:image"
-              content={process.env.NEXT_PUBLIC_OG_IMAGE}
-        />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-      </Head>
+          <meta name="twitter:card"
+                content="summary_large_image"
+          />
+          <meta property="twitter:url"
+                content={process.env.NEXT_PUBLIC_URL}
+          />
+          <meta name="twitter:title"
+                content={process.env.NEXT_PUBLIC_TITLE}
+          />
+          <meta name="twitter:description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
+          <meta name="twitter:image"
+                content={process.env.NEXT_PUBLIC_OG_IMAGE}
+          />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+        </Head>
 
-      <main>
-        Get insights that help your business grow.
-
-        Discover the benefits of data analytics and make better decisions regarding revenue, customer
-        experience, and overall efficiency.
-
-        10k+ companies
-        314 templates
-        12m+ queries
-      </main>
-    </>
-  )
+        <main>
+          <div className={styles.previewCardContainer}>
+            <PreviewCard/>
+          </div>
+        </main>
+      </>
+  );
 }
